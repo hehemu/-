8cb24f0846f140d2b1fea2376024e0a93018f1f4
hehemu <77370238+hehemu@users.noreply.github.com>
2021-01-13 15:47:09 +0800

    Update exp2.py

--- exp2/exp2.py
+++ exp2/exp2.py
@@ -1,99 +1,86 @@
 import pandas as pd
+import numpy as np
 import xlrd
-pd.set_option('display.max_columns', 1000)
-pd.set_option('display.width', 1000)
-pd.set_option('display.max_colwidth', 1000)
-one=pd.read_csv('学生数据1.txt',encoding='gbk')
-one['C10']=one['C10'].fillna(0) #将c0为空的属性全部添加为0
-Gender=one['Gender'].tolist()
-for i in range(len(Gender)):  #将female变为girl male变为boy
-    if Gender[i]=='female':
-        Gender[i] = 'girl';
-    if Gender[i]=='male':
-        Gender[i] = 'boy';
-one['Gender'] = Gender
-one['Height'] = one['Height'].apply(lambda x: float(x * 100)) #将Height的单位换算成cm
-one['ID']=one['ID'].apply(lambda x:str(x))  #将id属性变为字符串类型
-two=pd.read_excel('学生数据2.xlsx')
-two['C10']=two['C10'].fillna(0)
-ID=two['ID'].tolist()
-for i in range(len(ID)):  #转换ID为正确形式
-        ID[i] = 202000 + ID[i]
-two['ID'] = ID
-two['ID'] = two['ID'].apply(lambda x: str(x))  #将ID转换为str
-three=pd.merge(one,two,how='outer').drop_duplicates(subset=['ID']).sort_values('ID') #删除重复ID的
-three['Constitution']=three['Constitution'].fillna('bad')  #填充体育成绩
-#填充C1-C10所有空值的数据
-for i in range(1,11):
-    a = "C"+ str(i)
-    three[a].fillna(three[a].mean(),inplace=True)
-pe_grade = three['Constitution'].tolist()
-for i in range(len(pe_grade)):  #将标准改成百分制
-    if pe_grade[i] == 'excellent':
-        pe_grade[i] = 95
-    elif pe_grade[i] == 'good':
-        pe_grade[i] = 85
-    elif pe_grade[i] == 'general':
-        pe_grade[i]=75
-    elif pe_grade[i] == 'bad':
-        pe_grade[i] = 55
-three['Constitution']= pe_grade
-three.to_csv('合并数据.csv')  #合并数据在csv文件中
-
-
-#第一问
-beijing = three[three['City']=='Beijing']
-for i in range(1,11):
-    a = "C"+ str(i)
-    mean = sum(beijing[a])/len(beijing[a])
-    print('北京学生%s课程平均成绩 % .3f'% (a,mean))
-
-#第二问
-stu_num = three[(three['City']=='Guangzhou')&(three['C1']>=80)&(three['Gender']=='boy')&(three['C10']>=9)].shape[0]
-print('学生中家乡在广州，课程1在八十分以上，课程10在9分以上的男同学的数量为%d'%int(stu_num))
-
-#第三问
-Gz=three[(three['City']=='Guangzhou')&(three['Gender']=='girl')]
-Sh=three[(three['City']=='Shanghai')&(three['Gender']=='girl')]
-Gz_grade=Gz['Constitution'].tolist()
-Sh_grade=Sh['Constitution'].tolist()
-GZ_mean = sum(Gz_grade)/len(Gz_grade)
-Sh_mean = sum(Sh_grade)/len(Sh_grade)
-if GZ_mean>Sh_mean:
-    print('广州女生平均体能测试成绩比上海女生好\r\n平均体能测试成绩分别为%.3f、%.3f\r\n'% (GZ_mean,Sh_mean))
-elif GZ_mean==Sh_mean:
-    print('广州女生平均体能成绩与上海女生实力相当\r\n平均体能测试成绩分别为%.3f、%.3f\r\n'% (GZ_mean,Sh_mean))
-else:
-    print('广州女生平均体能成绩比上海女生差\r\n平均体能测试成绩分别为%.3f、%.3f\r\n'% (GZ_mean,Sh_mean))
-
-#第四问
+import matplotlib.pyplot as plt
+import heapq
+merge=pd.read_csv('合并数据.csv',encoding='gbk')
+x_values = merge['C1']
+y_values = merge['Constitution']
+plt.scatter(x_values, y_values, s=50)
+plt.xlabel('C1', fontsize=14)
+plt.ylabel('Constitution', fontsize=14)
+plt.show()
+plt.hist(x=merge['C1'],bins=8,edgecolor = 'black',range=[60,100])
+plt.xlabel('C1 mark', fontsize=14)
+plt.ylabel('time', fontsize=14)
+plt.show()
 for i in range(6,11):
     a = "C" + str(i)
-    three[a] = three[a].apply(lambda x:x*10)
+    merge[a] = merge[a].apply(lambda x:x*10)
 knowledge_mean = 0
 
-pe_grade = three['Constitution'].tolist()
+pe_grade = merge['Constitution'].tolist()
 pe_mean = sum(pe_grade) / len(pe_grade)
-each_pe_mean=three['Constitution'].apply(lambda x:x-pe_mean)
-print(each_pe_mean)
+each_pe_mean=merge['Constitution'].apply(lambda x:x-pe_mean)
 std_pr = pow(pow(each_pe_mean, 2).sum() / 105, 0.5)  # 体育成绩的方差
-print(std_pr)
-each_pe_mean = three['Constitution'].apply(lambda x:(x-pe_mean)/std_pr)
-print(each_pe_mean)
+each_pe_mean = merge['Constitution'].apply(lambda x:(x-pe_mean)/std_pr)
+
+each_mean = np.array([[0.0]*len(each_pe_mean)]*len(each_pe_mean))
+for x in range(0,len(each_pe_mean)):
+    each_mean[0][x] = round(each_pe_mean[x],3)
 
 
-for i in range(1,11):
+for i in range(1,10):
     a = "C" + str(i)
-    knowledge_grade = three[a].tolist()
+    knowledge_grade = merge[a].tolist()
     knowledge_mean = sum(knowledge_grade) / len(knowledge_grade)
-    each_aca_mean=three[a].apply(lambda x:x-knowledge_mean)
+    each_aca_mean=merge[a].apply(lambda x:x-knowledge_mean)
     std_aca = pow(pow(each_aca_mean, 2).sum() / 105, 0.5)
-    each_aca_mean=three[a].apply(lambda x:(x-knowledge_mean)/std_aca)
-    correlation=(each_aca_mean*each_pe_mean).sum()
-    print("课程%d和体能测试成绩的相关系数：%s" % (i,correlation))
-    if correlation<0.3:
-        print("得出结论：课程%d与体能成绩不相关"% i)
-    elif correlation>=0.3 and correlation<=0.8:
-        print("得出结论：课程%d与体能成绩弱相关"% i)
-    else:
-        print("得出结论：课程%d与体能成绩相关"% i)
+    each_aca_mean=merge[a].apply(lambda x:(x-knowledge_mean)/std_aca)
+    for x in range(0, len(each_aca_mean)):
+        each_mean[i][x] =  round(each_aca_mean[x],3)
+
+# for i in range(0,10):
+#     for j in range(0,len(each_pe_mean)):
+#         print(each_mean[i][j],end=" ")
+#     print()
+
+correlation_matrix = np.array([[0.0]*len(each_pe_mean)]*len(each_pe_mean))
+
+for i in range(0,len(each_pe_mean)):
+    for j in range(0,len(each_pe_mean)):
+        correlation = 0
+        for x in range(0,10):
+            correlation= correlation+each_mean[x][i]*each_mean[x][j]
+        correlation_matrix[i][j] = round(correlation,3)
+
+
+np.savetxt("correlation_matrix.txt",correlation_matrix,fmt="%d")
+
+
+plt.matshow(correlation_matrix,cmap=plt.get_cmap('Greens'), alpha=10000)  # , alpha=0.3
+plt.show()
+
+correlation_sample = np.array([[0]*3]*len(each_pe_mean))
+
+for i in range(0,len(each_pe_mean)):
+    max1_index = np.argmax(correlation_matrix[i])
+    max1_num = np.amax(correlation_matrix[i])
+    correlation_matrix[i][max1_index] = -1e9
+    max2_index = np.argmax(correlation_matrix[i])
+    max2_num = np.amax(correlation_matrix[i])
+    correlation_matrix[i][max2_index] = -1e9
+    max3_index = np.argmax(correlation_matrix[i])
+    max3num = np.amax(correlation_matrix[i])
+    correlation_matrix[i][max2_index] = max2_num
+    correlation_matrix[i][max1_index] = max1_num
+    correlation_sample[i][0] = merge['ID'][max1_index]
+    correlation_sample[i][1] = merge['ID'][max2_index]
+    correlation_sample[i][2] = merge['ID'][max3_index]
+
+
+np.savetxt("最近样本.txt",correlation_sample,fmt="%d")
+
+
+
+
